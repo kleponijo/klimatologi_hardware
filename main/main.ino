8f17e5cc0a02cd3ec9075940bc406dd37d6076b8
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-26 21:14:20 +0700

    beuh

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,29 @@
-int ledPin;
-String text = "Hello World!";
+#include <WiFi.h>
+
+const char* ssid = "DIDIET CH";
+const char* password = "didietch";
 
 void setup() {
-  serial.printl(text);
-  serial.begin(9600);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.println("connecting");
+
+  int timeout = 20;
+
+  while (WiFi.status() != WL_CONNECTED && timeout > 0) {
+    delay(500);
+    Serial.print(".");
+    timeout--;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP());
+} else {
+  Serial.println("Failed to connect!");
+}
+  
 }
 
 void loop() {
