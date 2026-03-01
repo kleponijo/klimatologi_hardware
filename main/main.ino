fdd76c00a68bd77cd4771733f5e5656b4574f898
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-03-01 19:02:08 +0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -14,17 +14,8 @@ volatile int pulseCount = 0;
 float K = 1.0;
 float kecepatan = 0;
 
-// realtime 1 detik
 unsigned long lastSecond = 0;
 const unsigned long intervalSecond = 1000;
-
-// history 1 jam
-unsigned long lastHour = 0;
-const unsigned long intervalHour = 3600000;
-
-// akumulasi rata-rata
-float totalKecepatan = 0;
-int jumlahSample = 0;
 
 void IRAM_ATTR hitungPulsa() {
   pulseCount++;
@@ -45,8 +36,7 @@ void setup() {
   Serial.println("\nWiFi Connected!");
   Serial.println(WiFi.localIP());
 
-  // Sinkron waktu NTP
-  configTime(7 * 3600, 0, "pool.ntp.org"); // GMT+7 Indonesia
+  configTime(7 * 3600, 0, "pool.ntp.org");
 
   attachInterrupt(digitalPinToInterrupt(pinHallEffect), hitungPulsa, FALLING);
 
@@ -55,18 +45,12 @@ void setup() {
 
 void loop() {
 
-  // =========================
-  // REALTIME PER DETIK
-  // =========================
   if (millis() - lastSecond >= intervalSecond) {
 
     int Rdetik = pulseCount;
     pulseCount = 0;
 
     kecepatan = 6.7824 * Rdetik * K;
-
-    totalKecepatan += kecepatan;
-    jumlahSample++;
 
     Serial.print("Realtime km/h: ");
     Serial.println(kecepatan);
@@ -75,32 +59,8 @@ void loop() {
 
     lastSecond = millis();
   }
-
-  // =========================
-  // HISTORY PER JAM
-  // =========================
-  if (millis() - lastHour >= intervalHour) {
-
-    if (jumlahSample > 0) {
-
-      float rataRata = totalKecepatan / jumlahSample;
-
-      Serial.print("Rata-rata 1 jam: ");
-      Serial.println(rataRata);
-
-      kirimHistory(rataRata, jumlahSample);
-
-      totalKecepatan = 0;
-      jumlahSample = 0;
-    }
-
-    lastHour = millis();
-  }
 }
 
-// =========================
-// FUNCTION REALTIME (PUT)
-// =========================
 void kirimRealtime(float kecepatan, int pulsa) {
 
   if (WiFi.status() == WL_CONNECTED) {
@@ -122,36 +82,6 @@ void kirimRealtime(float kecepatan, int pulsa) {
     int response = http.PUT(jsonData);
 
     Serial.print("Realtime HTTP: ");
-    Serial.println(response);
-
-    http.end();
-  }
-}
-
-// =========================
-// FUNCTION HISTORY (POST)
-// =========================
-void kirimHistory(float rataRata, int totalSample) {
-
-  if (WiFi.status() == WL_CONNECTED) {
-
-    HTTPClient http;
-    String url = String(firebaseHost) + "/anemometer/history_per_jam.json";
-
-    http.begin(url);
-    http.addHeader("Content-Type", "application/json");
-
-    time_t now = time(nullptr);
-
-    String jsonData = "{";
-    jsonData += "\"rata_rata\":" + String(rataRata, 2) + ",";
-    jsonData += "\"total_sample\":" + String(totalSample) + ",";
-    jsonData += "\"timestamp\":" + String(now);
-    jsonData += "}";
-
-    int response = http.POST(jsonData);
-
-    Serial.print("History HTTP: ");
     Serial.println(response);
 
     http.end();
